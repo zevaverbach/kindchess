2b4b7c9b51b8d25251349fbf7836e49288b284a1
Zev Averbach <zev@averba.ch>
2023-02-02 17:12:38 +0100

    the one needed Flask route is created

--- zevchess/api.py
+++ zevchess/api.py
@@ -1,8 +1,19 @@
-from zevchess.commands import update_existing_uids_cache
+import flask
+
+from zevchess.commands import update_existing_uids_cache, create_game as create_a_game
 from zevchess.queries import get_existing_uids_from_db
 
 # TODO: limit the number of concurrent games based on load testing
 # TODO: limit the number of games for a given IP address
+
+
+application = flask.Flask(__name__)
+
+
+@application.route("/create_game", methods=["POST"])
+def create_game() -> flask.Response:
+    uid = create_a_game()
+    return flask.jsonify({"uid": uid})
 
 
 def main():
