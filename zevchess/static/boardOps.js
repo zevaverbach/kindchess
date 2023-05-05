4dcbebbdc81e94d5f197cb02a28e4c9ac05c2db0
Zev Averbach <zev@averba.ch>
2023-05-05 13:51:49 +0200

    having moved 'side' to 'store', removed it from the arguments of all functions in boardOps and moveOps and imported 'store'

--- zevchess/static/boardOps.js
+++ zevchess/static/boardOps.js
@@ -1,83 +1,86 @@
+import {
+    store
+} from './store.js';
+
 export function getPieceAt(src, board) {
-  const piece = board.getPiece(src);
-  if (piece == null) return null;
-  const [color, pieceName] = piece;
-  return color === "b" ? pieceName : pieceName.toUpperCase();
+    const piece = board.getPiece(src);
+    if (piece == null) return null;
+    const [color, pieceName] = piece;
+    return color === "b" ? pieceName : pieceName.toUpperCase();
 }
 
-export function getKingStartSquare(side) {
-  return side === "black" ? "e8" : "e1"
+export function getKingStartSquare() {
+    return store.side === "black" ? "e8" : "e1"
 }
 
-function getLastRank(side) {
-  return side === "black" ? 1 : 8;
+function getLastRank() {
+    return store.side === "black" ? 1 : 8;
 }
 
-export function isPromotionMove(move, side) {
-  if (move.piece.toLowerCase() !== "p") return false;
-  const [_, rank] = move.dest;
-  return parseInt(rank) === getLastRank(side);
+export function isPromotionMove(move) {
+    if (move.piece.toLowerCase() !== "p") return false;
+    const [_, rank] = move.dest;
+    return parseInt(rank) === getLastRank();
 }
 
 export function invalidMove(move, possibleMoves) {
-  return !possibleMoves.some(pm => 
-    // double equals because some of these attributes may be 'undefined' on one side
-    // and null on the other
-    pm.src == move.src 
-    && pm.dest == move.dest 
-    && pm.piece == move.piece 
-    && pm.capture == move.capture 
-    && pm.castle == move.castle
-  )
+    return !possibleMoves.some(pm =>
+        // double equals because some of these attributes may be 'undefined' on one side
+        // and null on the other
+        pm.src == move.src &&
+        pm.dest == move.dest &&
+        pm.piece == move.piece &&
+        pm.capture == move.capture &&
+        pm.castle == move.castle
+    )
 }
 
 export function isCaptureMove(event, piece, board) {
-  const thereIsAPieceAtDest = getPieceAt(event.squareTo, board);
-  if (thereIsAPieceAtDest) {
-    return 1;
-  }
-  if (piece.toLowerCase() === "p") {
-    const [fileSource, rankSourceStr] = event.squareFrom;
-    const [fileDest, rankDestStr] = event.squareTo;
-    const fileSourceIndex = "abcdefgh".indexOf(fileSource);
-    const fileDestIndex = "abcdefgh".indexOf(fileDest);
-    const rankSource = parseInt(rankSourceStr);
-    const rankDest = parseInt(rankDestStr);
-    if (
-      Math.abs(fileSourceIndex - fileDestIndex) === 1 
-      && Math.abs(rankDest - rankSource) === 1
-     ) { 
-      return 1;
+    const thereIsAPieceAtDest = getPieceAt(event.squareTo, board);
+    if (thereIsAPieceAtDest) {
+        return 1;
     }
-  }
-  return 0;
+    if (piece.toLowerCase() === "p") {
+        const [fileSource, rankSourceStr] = event.squareFrom;
+        const [fileDest, rankDestStr] = event.squareTo;
+        const fileSourceIndex = "abcdefgh".indexOf(fileSource);
+        const fileDestIndex = "abcdefgh".indexOf(fileDest);
+        const rankSource = parseInt(rankSourceStr);
+        const rankDest = parseInt(rankDestStr);
+        if (
+            Math.abs(fileSourceIndex - fileDestIndex) === 1 &&
+            Math.abs(rankDest - rankSource) === 1
+        ) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 export function isCastlingMove(event, piece) {
-  return (
-    piece.toLowerCase() === "k" 
-    && (event.squareFrom[0] === "e" 
-      && ["g", "c"].includes(event.squareTo[0]))
-  );
+    return (
+        piece.toLowerCase() === "k" &&
+        (event.squareFrom[0] === "e" && ["g", "c"].includes(event.squareTo[0]))
+    );
 }
 
-export function isEnPassantMove(move, board, side) {
-  if (!move.dest) {
-    return false;
-  }
-  const [file, rankStr] = move.dest;
-  const rank = parseInt(rankStr);
-  const spaceBehindDest = side === "black" ? `${file}${rank + 1}` : `${file}${rank - 1}`;
-  return (
-    move.piece.toLowerCase() == "p" 
-    && move.capture === 1 
-    && getPieceAt(spaceBehindDest, board) == toggleCase(move.piece)
-  );
+export function isEnPassantMove(move, board) {
+    if (!move.dest) {
+        return false;
+    }
+    const [file, rankStr] = move.dest;
+    const rank = parseInt(rankStr);
+    const spaceBehindDest = store.side === "black" ? `${file}${rank + 1}` : `${file}${rank - 1}`;
+    return (
+        move.piece.toLowerCase() == "p" &&
+        move.capture === 1 &&
+        getPieceAt(spaceBehindDest, board) == toggleCase(move.piece)
+    );
 }
 
 function toggleCase(str) {
-  if (str.toUpperCase() === str) {
-    return str.toLowerCase();
-  }
-  return str.toUpperCase();
+    if (str.toUpperCase() === str) {
+        return str.toLowerCase();
+    }
+    return str.toUpperCase();
 }
--- zevchess/static/moveOps.js
+++ zevchess/static/moveOps.js
@@ -1,51 +1,57 @@
-import { getPieceAt } from './boardOps.js';
+import {
+    getPieceAt
+} from './boardOps.js';
 
-export function doTheMoveSentCastle(move, side, board) {
-  const rank = side === "black" ? 8 : 1; // it's this side that castled
-  if (move.castle === "k") {
-    board.movePiece(`h${rank}`, `f${rank}`, true)
-  } else {
-    board.movePiece(`a${rank}`, `d${rank}`, true)
-  }
-}
+import {
+    store,
+} from './store.js';
 
-export function doTheMoveSentEnPassant(move, board, side) {
-  const [file, rankStr] = move.dest;
-  const rank = parseInt(rankStr);
-  const sq = side === "black" ? `${file}${rank + 1}` : `${file}${rank - 1}`;
-  board.setPiece(sq, null);
-}
-
-export function doTheMoveReceived(move, board, side) {
-  let from, to;
-  if (move.castle) {
-    const rank = side === "black" ? 1 : 8; // it's the other side that castled
+export function doTheMoveSentCastle(move, board) {
+    const rank = store.side === "black" ? 8 : 1; // it's this side that castled
     if (move.castle === "k") {
-      [from, to] = [`e${rank}`, `g${rank}`];
-      board.movePiece(from, to, true)
-      board.movePiece(`h${rank}`, `f${rank}`, true)
+        board.movePiece(`h${rank}`, `f${rank}`, true)
     } else {
-      [from, to] = [`e${rank}`, `c${rank}`];
-      board.movePiece(from, to, true)
-      board.movePiece(`a${rank}`, `d${rank}`, true)
+        board.movePiece(`a${rank}`, `d${rank}`, true)
     }
-    return [from, to]
-  }
-  [from, to] = [move.src, move.dest];
-  const enPassant = move.capture === 1 && getPieceAt(to, board) == null
-  board
-    .movePiece(from, to, true).then(() => {
-      if (enPassant) {
-        const [file, rankStr] = move.dest;
-        const rank = parseInt(rankStr);
-        const sq = side === "black" ? `${file}${rank - 1}` : `${file}${rank + 1}`;
-        board.setPiece(sq, null);
-      // TODO: change this since there will noly be move.promote == 1
-      } else if (move.promotion_piece) {
-        const pp = move.promotion_piece;
-        const piece = pp.toUpperCase() === pp ? `w${pp.toLowerCase()}` : `b${pp}`;
-        board.setPiece(move.dest, piece);
-      }
-  });
-  return [from, to];
+}
+
+export function doTheMoveSentEnPassant(move, board) {
+    const [file, rankStr] = move.dest;
+    const rank = parseInt(rankStr);
+    const sq = store.side === "black" ? `${file}${rank + 1}` : `${file}${rank - 1}`;
+    board.setPiece(sq, null);
+}
+
+export function doTheMoveReceived(move, board) {
+    let from, to;
+    if (move.castle) {
+        const rank = store.side === "black" ? 1 : 8; // it's the other side that castled
+        if (move.castle === "k") {
+            [from, to] = [`e${rank}`, `g${rank}`];
+            board.movePiece(from, to, true)
+            board.movePiece(`h${rank}`, `f${rank}`, true)
+        } else {
+            [from, to] = [`e${rank}`, `c${rank}`];
+            board.movePiece(from, to, true)
+            board.movePiece(`a${rank}`, `d${rank}`, true)
+        }
+        return [from, to]
+    }
+    [from, to] = [move.src, move.dest];
+    const enPassant = move.capture === 1 && getPieceAt(to, board) == null
+    board
+        .movePiece(from, to, true).then(() => {
+            if (enPassant) {
+                const [file, rankStr] = move.dest;
+                const rank = parseInt(rankStr);
+                const sq = store.side === "black" ? `${file}${rank - 1}` : `${file}${rank + 1}`;
+                board.setPiece(sq, null);
+                // TODO: change this since there will noly be move.promote == 1
+            } else if (move.promotion_piece) {
+                const pp = move.promotion_piece;
+                const piece = pp.toUpperCase() === pp ? `w${pp.toLowerCase()}` : `b${pp}`;
+                board.setPiece(move.dest, piece);
+            }
+        });
+    return [from, to];
 }
